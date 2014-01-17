26d1bf5c7b8bcb5b4f24a379a2b92a69d1f3c732
root <ethand320@yahoo.com>
2014-01-17 12:50:52 -0600

    added towing app

---
+++ tow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import sys
+import smtplib
+import re
+import urllib
+import urllib2
+import time
+import logging
+from subprocess import Popen, PIPE
+license = "78731FF"
+
+data = { "{actionForm.plateNumber}" : license }
+
+encoded_data = urllib.urlencode(data)
+
+content = urllib2.urlopen("https://webapps1.cityofchicago.org/vehicleSearch/org/cityofchicago/vs/controller/search/searchPlate.do", encoded_data)
+
+
+
+
+str1 = content.read()
+#print str1 
+#print str.find("<label>CHEV</label>")
+today =  time.strftime("%x")
+
+logging.basicConfig(filename='/var/log/towlog.out',level=logging.DEBUG)
+
+if today in str1:
+	print "ethan is screwed"
+	logging.debug('ethan is screwed!')
+else:
+    print "No worky"
+    logging.debug('Nothing to report...')
+
+#if "10/05/2013" in str1:
+#	print "it worked"
+
+
+
+
